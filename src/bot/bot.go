57c733edf740ca62b6470667988fe850105ef85e
DarkHole1 <dark.hole1@yandex.ru>
2019-05-02 07:46:14 +0300

    Added command parsing

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"fmt"
+	"regexp"
 	"sync"
 
 	"../config"
@@ -17,19 +18,42 @@ func Start(wg *sync.WaitGroup, users *musers.Users, api *slack.Client, config *c
 
 	fmt.Println("Bot started!")
 
+	// Оставить комментарий к задаче
+	pattern := regexp.MustCompile(`^Оставить комментарий "([^"]+)" к задаче (\S+)$`)
+	// Оставить затраченное время и комментарий
+	pattern2 := regexp.MustCompile(`^Оставить затраченное время (\d+) часов и комментарий "([^"]+)" к задаче (\S+)$`)
+	// Взять задачу с комментарием
+	pattern3 := regexp.MustCompile(`^Взять задачу (\S+) с комментарием "([^"]+)"$`)
+	// Перевести задачу в состояние готово
+	pattern4 := regexp.MustCompile(`^Перевести задачу (\S+) в состояние готово$`)
+	// Перевести задачу на другого пользователя
+	pattern5 := regexp.MustCompile(`^Перевести задачу (\S+) на пользователя (\S+)$`)
+
 	for message := range api.GetMessages() {
-		// Оставить комментарий к задаче
-		// pattern := regexp.MustCompile("^Оставить комментарий \"([^\"]+)\" к задаче (\\S+)$")
-		// Оставить затраченное время и комментарий
-		// Взять задачу с комментарием
-		// Перевести задачу в состояние готово
-		// Перевести задачу на другого пользователя
 		user := users.FindBySlackID(musers.SlackID(message.User))
 		user.SlackChannal = string(message.Channel)
-		if user.OauthToken == "" {
+		// if user.OauthToken == "" {
+		if false {
 			api.SendMessage("Need oauth https://www.wrike.com/oauth2/authorize/v4?client_id="+config.Wrike.ID+"&response_type=code", message.Channel)
 		} else {
-			api.SendMessage("Hello", message.Channel)
+			if match := pattern.FindStringSubmatch(message.Text); match != nil {
+				api.SendMessage("Comment on task "+match[2]+" left", message.Channel)
+
+			} else if match := pattern2.FindStringSubmatch(message.Text); match != nil {
+				api.SendMessage("Comment on task "+match[3]+" left", message.Channel)
+
+			} else if match := pattern3.FindStringSubmatch(message.Text); match != nil {
+				api.SendMessage("Take task "+match[1], message.Channel)
+
+			} else if match := pattern4.FindStringSubmatch(message.Text); match != nil {
+				api.SendMessage("Task "+match[1]+" finished", message.Channel)
+
+			} else if match := pattern5.FindStringSubmatch(message.Text); match != nil {
+				api.SendMessage("Moved task "+match[1]+" on user "+match[2], message.Channel)
+
+			} else {
+				api.SendMessage("Unrecongonized command", message.Channel)
+			}
 		}
 	}
 }
